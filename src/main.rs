0a5eef84fbf63212d480e972c84c86f3e8aae70e
AP Ljungquist <anlj@magenta.local>
2025-02-14 18:14:36 +0100

    r: refresh my memory on the interface

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use std::collections::HashMap;
+
+macro_rules! map (
+    { $($key:expr => $value:expr),+$(,)? } => {
+        {
+            let mut result = HashMap::new();
+            $(
+                result.insert($key, $value);
+            )+
+            result
+        }
+     };
+);
+
+fn main() {
+    let tree = map! {
+        "D" => vec!["A"],
+        "I" => vec!["A", "G"],
+        "A" => vec!["V"],
+        "G" => vec!["V"],
+        "V" => vec![],
+    };
+    let order = vec!["D", "I", "A", "G", "V"];
+    println!("{}", diagvrs::formatted(&tree, &order));
+}
